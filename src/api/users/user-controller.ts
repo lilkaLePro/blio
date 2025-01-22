f2a46283eccd8eb4bd430384ba04a7bee1b31236
Kaly DIALLO <kalydiallo@192.168.1.9>
2025-01-22 18:14:41 +0000

    fix register function

--- src/api/users/user-controller.ts
+++ src/api/users/user-controller.ts
@@ -44,26 +44,33 @@ export const getUserByToken = async (req: Request, res: Response) => {
 export const register = async (req: Request, res: Response) => {
   try {
     const { email, firstname, lastname, password } = req.body;
-
+    if (!email || !firstname || !lastname || !password) {
+      console.log(email, firstname, lastname, email);
+      
+      return res.status(400).json({ msg: 'All fields are required' });
+    }
     const existingUser = await getUserByEmail(email);
     if (existingUser) {
       return res.status(400).json({ msg: 'user exist deja' });
     }
-
     const saltString = random();
+    const hashedPw = await authentication(saltString, password);
     const user = await createUser({
       email,
       firstname,
       lastname,
-      password: authentication(saltString, password),
+      password: hashedPw,
       sessionToken: null,
       sessionTokenExpiresAt: null,
       salt: saltString,
     });
     const id = user?._id as string;
-    const sessionToken = authentication(saltString, id.toString() );
+    if (!id) {
+      throw new Error('User ID is missing');
+    }
+    const sessionToken = await authentication(saltString, id.toString() );
     let token = user?.sessionToken;
-    token = sessionToken.toUpperCase();
+    token =  sessionToken.toUpperCase();
 
     const expirationDuration = 24 * 60 * 60 * 1000;
     const sessionTokenExpiresAt = new Date(expirationDuration + Date.now());
@@ -73,13 +80,13 @@ export const register = async (req: Request, res: Response) => {
     await updateUserById(id, { sessionToken, sessionTokenExpiresAt });
 
     if (key) {
-      return res.cookie(key, sessionToken, { domain: 'localhost', path: '/' });
+      res.cookie(key, sessionToken, { domain: 'localhost', path: '/' });
     }
 
     return res.status(200).json({ msg: 'user created successfully', user, token, expireAt });
   } catch (error) {
-    console.error(error);
-    return res.sendStatus(500).json({ error: 'user creation failed' });
+    console.log(error);
+    return res.status(500).json('user creation failed');
   }
 };
 
@@ -96,7 +103,7 @@ export const login = async (req: Request, res: Response) => {
       password: verifedEmail?.password,
       salt: verifedEmail?.salt,
     };
-    const verifedPassword = authentication(userData?.salt, password);
+    const verifedPassword = await authentication(userData?.salt, password);
     if (userData?.password !== verifedPassword) {
       return res.status(403).json('Password incorrect');
     }
